f5b5fa63225310141d16c636c0cdba8605aa9463
Eda Işıl İnal <edainalisil@gmail.com>
2024-06-27 11:25:17 +0300

    add github and linkedin links in footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import './style.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
 
 function Footer() {
   return (
-    <div >
-      This is a footer
+    <div className='footer' >
+   
+      <div className='icons'>
+     
+    <a href="https://github.com/Eda-Inal" target="_blank" rel="noopener noreferrer">
+    <FontAwesomeIcon icon={faGithub} size="2x" />
+    </a>
+    <a href="https://www.linkedin.com/in/eda-i%C5%9F%C4%B1l-inal-887a69267//" target="_blank" rel="noopener noreferrer"> 
+         <FontAwesomeIcon icon={faLinkedin} size="2x" />
+        </a>
+      
+      </div>
+  
     </div>
   )
 }
