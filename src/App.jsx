e79cc97fc9f6ec1fe943c6a497c609b615ec9454
Eda Işıl İnal <edainalisil@gmail.com>
2024-06-27 11:00:25 +0300

    Update component to dispatch Redux actions and use selectors

--- src/App.jsx
+++ src/App.jsx
@@ -1,74 +1,80 @@
 import logo from './logo.svg';
 import './App.css';
-import { useSelector,useDispatch } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { fetchData } from './redux/paraSlice';
-import { useEffect, useState,useRef } from 'react';
+import { useEffect, useState, useRef } from 'react';
+import { handleSelectedValue } from './redux/paraSlice';
 
 function App() {
- const [numParagraphs, setNumParagraphs] = useState(4);
- const prevNumParagraphsRef = useRef(0);
- const [selectedValue, setSelectedValue] = useState('no');
- const [yes, setYes] = useState(false);
- const dispatch = useDispatch();
- const { loading, data, error } = useSelector((state) => state.paras);
- useEffect(() => {
-  if (numParagraphs !== prevNumParagraphsRef.current) {
-    dispatch(fetchData(numParagraphs));
-    prevNumParagraphsRef.current = numParagraphs;
-  }
-}, [numParagraphs, dispatch]);
- console.log(data);
- const handleSelectValue = (e) => {
-setSelectedValue(e.target.value)
-yes ? setYes(false) : setYes(true)
+  const [numParagraphs, setNumParagraphs] = useState(4);
+  const prevNumParagraphsRef = useRef(0);
+  //  const [selectedValue, setSelectedValue] = useState('no');
+  //  const [yes, setYes] = useState(false);
+  const dispatch = useDispatch();
+  const { loading, data, error, selectedValue, isYes } = useSelector((state) => state.paras);
 
- }
- console.log(yes);
- 
- 
+  console.log(selectedValue, isYes);
+  useEffect(() => {
+    if (numParagraphs !== prevNumParagraphsRef.current) {
+      dispatch(fetchData(numParagraphs));
+      prevNumParagraphsRef.current = numParagraphs;
+    }
+  }, [numParagraphs, dispatch]);
+  console.log(data);
+  //  const handleSelectValue = (e) => {
+  // setSelectedValue(e.target.value)
+  // yes ? setYes(false) : setYes(true)
+
+  //  }
+  //  console.log(yes);
+  const handleSelectChange = (e) => {
+    dispatch(handleSelectedValue(e.target.value));
+  };
+
+
 
   return (
     <div className="App">
-<h1 className='h1'>Text Generator App</h1>
-<div className='box'>
-  <div className='b1'>
-    <div>Paragraph</div> <br />
-    <div><input type="number" 
-value={numParagraphs}
-min="1"
-onChange={(e)=> setNumParagraphs(e.target.value)}
-/></div>
-  </div>
+      <h1 className='h1'>Text Generator App</h1>
+      <div className='box'>
+        <div className='b1'>
+          <div>Paragraph</div> <br />
+          <div><input type="number"
+            value={numParagraphs}
+            min="1"
+            onChange={(e) => setNumParagraphs(e.target.value)}
+          /></div>
+        </div>
 
-  <div className='b2'>
-    <div>Include HTML</div> <br />
-    <div >
-      <select className='dropdown' value={selectedValue} onChange={handleSelectValue}>
-      <option value="yes" >Yes</option>
-      <option value="no"  >No</option>
-      </select>
-    </div>
-  </div>
-</div>
+        <div className='b2'>
+          <div>Include HTML</div> <br />
+          <div >
+            <select className='dropdown' value={selectedValue} onChange={handleSelectChange} >
+              <option value="yes" >Yes</option>
+              <option value="no"  >No</option>
+            </select>
+          </div>
+        </div>
+      </div>
 
 
-  {error && <p>Error: {error}</p>}
+      {error && <p>Error: {error}</p>}
 
-<div className='p'>
-  {data.map((paragraph, index) => {
-    const isEven = index % 2 === 0;
-   
-    return isEven && yes ? (
-      <p key={index}>
-        &lt;p&gt;{paragraph}&lt;/p&gt;
-      </p>
-    ) : (
-      <p key={index}>{paragraph}</p>
-    );
-  })}
-</div>
+      <div className='p'>
+        {data.map((paragraph, index) => {
+          const isEven = index % 2 === 0;
 
-            
+          return isEven && isYes ? (
+            <p key={index}>
+              &lt;p&gt;{paragraph}&lt;/p&gt;
+            </p>
+          ) : (
+            <p key={index}>{paragraph}</p>
+          );
+        })}
+      </div>
+
+
 
     </div>
   );
--- src/redux/paraSlice.jsx
+++ src/redux/paraSlice.jsx
@@ -1,23 +1,30 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 
-export const fetchData = createAsyncThunk('data/fetchData',async(numParagraphs) => {
-  
+export const fetchData = createAsyncThunk('data/fetchData', async (numParagraphs) => {
+
     const response = await fetch(`https://baconipsum.com/api/?type=meat-and-filler&paras=${numParagraphs}&format=text`);
- 
+
 
     const data = await response.text();
-    return data.split('\n'); 
-    
+    return data.split('\n');
+
 });
 
 export const paraSlice = createSlice({
-    name : 'paras' ,
-    initialState:{
+    name: 'paras',
+    initialState: {
         loading: false,
         data: [],
-        error: ''
+        error: '',
+        selectedValue: 'no',
+        isYes: false
     },
-    reducers : {},
+    reducers: {
+        handleSelectedValue: (state, action) => {
+            state.selectedValue = action.payload
+            state.isYes ? state.isYes = false : state.isYes = true
+        }
+    },
     extraReducers: (builder) => {
         builder
             .addCase(fetchData.pending, (state) => {
@@ -35,5 +42,5 @@ export const paraSlice = createSlice({
             });
     }
 })
-
+export const { handleSelectedValue } = paraSlice.actions
 export default paraSlice.reducer;
